cdbcad6a6c422891f31e04da4ad28d052df136c4
Piotr Stolarz <pstolarz@o2.pl>
2023-03-21 00:07:23 +0100

    src/lifetime.rs update

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -1,15 +1,51 @@
+fn _f<'a, 'b>(_a: &'a i32, mut _b: &'b i32)
+where 'a: 'b
+{
+    // ok: &'a i32 is subtype of &'b i32 (therefore may coerce into it)
+    _b = _a;
+
+    // &'b &'a i32 is well formed (coercion from base to subtype)
+    let _r: &'b &'a i32 = &&0;
+}
+
+// ----------------------------------------------------------------------------
+
+fn choose_first<'a: 'b, 'b>(first: &'a i32, _: &'b i32) -> &'b i32 {
+    first
+}
+
+fn test_1() {
+    let x = 2; // longer lifetime ('a)
+    let mut res: &i32;
+
+    {
+        let y = 3; // shorter lifetime ('b)
+
+        // 'a: 'b, so their fits choose_first() arguments w/o coercion
+        res = choose_first(&x, &y);
+        println!("{} is the first (1)", res);
+
+        // 'b: 'a, therefore 'a is coerced into 'b,
+        // so both choose_first() args have the same lifetime 'b
+        res = choose_first(&y, &x);
+        println!("{} is the first (2)", res);
+    };
+}
+
+// ----------------------------------------------------------------------------
+
 fn max<'a, 'b, 'c>(a: &'a i32, b: &'b i32, c: &'c i32) -> &'c i32
 where
-    // a,b data may flow into c
+    // 'a,'b may coerce `c
     'a: 'c, 'b: 'c
 
     // Alternatively:
-    // b may flow into a, and a into c, so a,b may flow into c
+    // 'b may coerce into 'a, and 'a into 'c
     // 'b: 'a, 'a: 'c
-    // a may flow into b, and b into c, so a,b may flow into c
+    // 'a may coerce into 'b, and 'b into 'c
     // 'a: 'b, 'b: 'c
 
-    // but error here (a may flow into c but not b):
+    // but error here ('a may coerce into 'c but not 'b):
     // 'a: 'b, 'a: 'c
 {
     let ab = if a > b { a } else { b };
@@ -22,17 +58,7 @@ fn max2<'a>(a: &'a i32, b: &'a i32, c: &'a i32) -> &'a i32
     if ab > c { ab } else { c }
 }
 
-fn _f<'a, 'b>(_a: &'a i32, mut _b: &'b i32)
-where 'a: 'b
-{
-    // ok: a data may flow into b
-    _b = _a;
-
-    // &'b &'a i32 is well formed because 'a: 'b
-    let _r: &'b &'a i32 = &&0;
-}
-
-pub fn test()
+fn test_2()
 {
     let a = 1;
     let res: &i32;
@@ -58,4 +84,12 @@ pub fn test()
     }
     // ERROR: refs does not live long enough
     // res; res2;
+}
+
+// ----------------------------------------------------------------------------
+
+pub fn test()
+{
+    test_1();
+    test_2();
 }
