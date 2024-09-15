a5675f786d15b4ead1a98297189d0539e51e9798
Piotr Stolarz <pstolarz@o2.pl>
2024-09-15 17:30:55 +0200

    lifetime.rs, refs.rs updated

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -1,16 +1,21 @@
 fn _f<'a, 'b>(_a: &'a i32, mut _b: &'b i32)
 where 'a: 'b
 {
-    // ok: &'a i32 is subtype of &'b i32 (therefore may coerce into it)
+    // ok: 'a has longer lifetime than 'b i32 (therefore may coerce to it)
     _b = _a;
 
-    // &'b &'a i32 is well formed (coercion from subtype to base)
+    // &'b &'a i32 is well formed (coercion from loger to shorter lifetime)
     let _r: &'b &'a i32 = &&0;
 }
 
 // ----------------------------------------------------------------------------
 
 fn choose_first<'a: 'b, 'b>(first: &'a i32, _: &'b i32) -> &'b i32 {
+    first
+}
+
+// better apprach
+fn choose_first2<'a>(first: &'a i32, _: &'a i32) -> &'a i32 {
     first
 }
 
@@ -24,11 +29,13 @@ fn test_1() {
         // 'a: 'b, so their fits choose_first() arguments w/o coercion
         res = choose_first(&x, &y);
         println!("{} is the first (1)", res);
+        assert!(res == choose_first2(&x, &y));
 
-        // 'b: 'a, therefore 'b is coerced into 'a,
+        // 'b: 'a, therefore 'b is coerced to 'a,
         // so both choose_first() args have the same lifetime 'a
         res = choose_first(&y, &x);
         println!("{} is the first (2)", res);
+        assert!(res == choose_first2(&y, &x));
     };
 }
 
@@ -36,22 +43,23 @@ fn test_1() {
 
 fn max<'a, 'b, 'c>(a: &'a i32, b: &'b i32, c: &'c i32) -> &'c i32
 where
-    // 'a,'b may coerce `c
+    // 'a,'b may coerce to `c
     'a: 'c, 'b: 'c
 
     // Alternatively:
-    // 'b may coerce into 'a, and 'a into 'c
+    // 'b may coerce to 'a, and 'a into 'c
     // 'b: 'a, 'a: 'c
-    // 'a may coerce into 'b, and 'b into 'c
+    // 'a may coerce to 'b, and 'b into 'c
     // 'a: 'b, 'b: 'c
 
-    // but error here ('a may coerce into 'c but not 'b):
+    // but error here ('a may coerce to 'c but not 'b):
     // 'a: 'b, 'a: 'c
 {
     let ab = if a > b { a } else { b };
     if ab > c { ab } else { c }
 }
 
+// better approach
 fn max2<'a>(a: &'a i32, b: &'a i32, c: &'a i32) -> &'a i32
 {
     let ab = if a > b { a } else { b };
@@ -73,7 +81,7 @@ fn test_2()
             // Both max() and max2() are equivalent
             //
             // NOTE: Regardless of lifetimes ranges passed to max() the
-            // lifetimes are confined to shortest lifetime c to fulfill
+            // lifetimes are coerced to shortest lifetime c to fulfill
             // bounds as specified by the where-clause.
             res = max(&c, &b, &a);
             res2 = max2(&c, &b, &a);
--- src/refs.rs
+++ src/refs.rs
@@ -4,30 +4,36 @@ pub fn test() {
     let r = &mut m;
     *r = *r + 1;
 
-    // ERROR: m is mutable borrowed by r
+    // ERROR: m is mutable borrowed by r, bute r is used below
     //println!("{}", m);
 
     // destructuring r (&mut i32) into mutable m2; same as: let mut m2 = *r;
-    let &mut mut m2 = r;    // m2 = m (copy)
+    let &mut mut m2 = r;    // m2 is a copy
     m2 = m2 - 1;
     assert!(m == 1 && m2 == 0);
 
     // same as: let r2 = &mut m;
-    // no idea how to declare let mut r2 = &mut m; using this syntax
     let ref mut r2 = m;
     *r2 = *r2 + 1;  // m == 2
 
     // ERROR: r2 is immutable
     //r2 = &mut m;
 
-    // r2 mutable ref ends here, m may be accessed
+    // r2 lifetime ends here, m may be accessed
     assert!(m == 2);
 
     // destructuring &mut i32 into mutable ref m
-    // same as: let r3 = &mut m;
+    // same as: let r3 = &mut m OR let ref mut r3 = m;
     let &mut ref mut r3 = &mut m;
     *r3 = *r3 + 1;
 
-    // r3 mutable ref ends here, m may be accessed
+    // r3 liefetime ends here, m may be accessed
     assert!(m == 3);
+
+    //ERROR: mutable by-reference bindings are experimental
+    //let mut ref r4 = m;
+
+    // borrow m by immutable reference
+    let r4 = &m;
+    assert!(*r4 == 3);
 }
